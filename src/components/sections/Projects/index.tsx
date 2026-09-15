8c0e9baea44e9b2e438aa0390d88b007d4970a3e
Arik <arikgevorgyan23@gmail.com>
2026-09-15 16:37:44 +0400

    feat: projects skeletons while loading

--- src/components/sections/Projects/index.tsx
+++ src/components/sections/Projects/index.tsx
@@ -21,7 +21,17 @@ const Projects = () => {
     >
       <div className="projects__list grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {status === "loading" ? (
-          <Skeleton />
+          <>
+            <div className="projects__item flex">
+              <Skeleton className="w-full h-145 rounded-xl" />
+            </div>
+            <div className="projects__item flex">
+              <Skeleton className="w-full h-145 rounded-xl" />
+            </div>
+            <div className="projects__item flex">
+              <Skeleton className="w-full h-145 rounded-xl" />
+            </div>
+          </>
         ) : (
           projects?.map((project) => (
             <div className="projects__item flex" key={project.id}>
