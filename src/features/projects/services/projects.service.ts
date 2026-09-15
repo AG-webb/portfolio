56f7414a67e8db6e489a8021837a700bf4eb412b
Arik <arikgevorgyan23@gmail.com>
2026-09-15 21:15:57 +0400

    feat: projects list order changes

--- src/features/projects/services/projects.service.ts
+++ src/features/projects/services/projects.service.ts
@@ -2,7 +2,11 @@ import { supabase } from "@/lib/supabase/supabase";
 import type { Project } from "../types";
 
 export const getProjectsService: () => Promise<Project[] | null> = async () => {
-  const { data: projects, error } = await supabase.from("projects").select("*");
+  const { data: projects, error } = await supabase
+    .from("projects")
+    .select("*")
+    .order("created_at", { ascending: true })
+    // .range(0, 2);
 
   if (error) throw error;
 
